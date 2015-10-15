83b4e1696f7e3685c7de02ca4e592e3b53a32308
Kazanskiy Sergey <skaz@inbox.ru>
2015-10-15 17:31:32 +0300

    Added "catch_input" core function

--- reports.sh
+++ reports.sh
@@ -1,9 +1,38 @@
-#!/bin/bash
+#!/bin/sh
 ## change it to location of reports.config
 source /etc/zabbix/reports/config
+function catch_input() {
+case $2 in
+	text)
+		echo -ne "${1} "
+		read -r text
+		[[ "${text}" == "" ]] && return 1 || return 0	
+	;;
+	password)
+		echo -ne "${1}"
+		read -rs password
+		[[ "${password}" == "" ]] && return 1 || return 0
+	;;
+	*)
+		echo -ne "${1}"
+		read -r decision
+		case $decision in
+			[Yy]) return 0;;
+			*) return 1;;
+		esac
+	;;
 
-function cookie {
-${bindir}/zcookie $@
+esac
+}
+export -f catch_input
+function usage {
+export silent=1
+cookie check
+case $? in
+        0) echo "usage $0: [ generate | checkcookie | cookie ]";;
+        1) catch_input "No cookie file was found!\nDo you want to get cookie now?\t [Y/n]\t" && cookie get||echo "Bye";; 
+        2) catch_input "No cookie in file!\nDo you want to get cookie now?\t [Y/n]\t" && cookie get||echo "Bye";;
+esac
 }
 
 function get_graph {
@@ -11,7 +40,14 @@ wget -4 --load-cookies=${cookieFile} -O ${tempdir}/${1}.jpg "${ZabbixHost}/chart
 }
 
 function main {
-cd ${tempdir}
+export silent=1
+cookie check
+case $? in
+	0) echo "all ok";;
+	1) echo "no file, aborting" && exit 1;;
+	2) echo "no cookie in file" && exit 2;;
+esac
+exit 0
 for ReportName in `ls ${basedir}/enabled`; do
 	source ${basedir}/enabled/${ReportName};
 
@@ -29,13 +65,13 @@ for ReportName in `ls ${basedir}/enabled`; do
 	get_graph `echo ${graphid}|awk -F\: '{ print $1" "(length($2)<"2"?"900":$2)" "(length($3)<"2"?"150":$3)}'` 
 	done
 
-## create mail body and sendit! ##
+## Processing email ##
 echo start
 source ${templates}/${Body}
 echo "here we are!"
 MakeBody|SendMail
 echo end
-	unset -v Type Graphs SendTo Subject Body Period Cycle
+	unset -v Type Graphs From SendTo Subject Body Period Cycle
 done
 }
 
@@ -43,4 +79,5 @@ case ${1} in
 	generate) main;;
 	checkcookie)	check_cookie;;
 	cookie)	cookie ${2} ${3};;
+	*) usage;;
 esac
